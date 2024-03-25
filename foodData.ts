1f9409305b44ec05609c07005935bea0a2963a56
Justin Lang <jlang61@ucsb.edu>
2024-03-25 23:40:42 +0000

    Update foodData.ts

--- foodData.ts
+++ foodData.ts
@@ -1,189 +1,26 @@
-Dining Area: Carrillo
-Category: Mongolian Grill
-- Sticky Rice (vgn)
-- Teriyaki Beef
-- Stir Fry Noodles with Vegetables (vgn)
-Category: Salads
-- Caesar Salad
-Category: Euro
-- Herb Marinated Chicken Breast Sandwich
-- Baby Carrots with Tarragon (vgn)
-- Corn (vgn)
-- Mashed Potatoes (v)
-Category: Pasta
-- Tomato Rice Soup (vgn)
-- Linguini Pasta (vgn)
-- Tomato Basil & Garlic Sauce (vgn)
-- Whole Wheat Penne Pasta (vgn)
-- Creamy Rosemary Sauce (v)
-- Black Beans (vgn)
-- Baked Potato (vgn)
-- Baked Sweet Potato (vgn)
-- Wild and Long Grain Rice Blend (vgn)
-Category: Pizza
-- Pepperoni Pizza
-- Tomato Basil Pizza on Wheat (v)
-- Cheese Pizza on White (v)
-Category: Bakery
-- Whole Wheat Roll (vgn)
-- Banana Macadamia Nut Cake (w/nuts) (v)
-
-Dining Area: De La Guerra
-Category: Blue Plate Special
-- Vegetable Noodle Soup (vgn)
-- Roast Beef
-- Rice Pilaf (vgn)
-- Vegetable Medley (vgn)
-- Baked Potato (vgn)
-Category: Taqueria (East Side)
-- Chicken Sope
-- Black Bean Tostada (v)
-- Green Rice (vgn)
-- Black Beans (vgn)
-- Flour Tortilla (vgn)
-- Corn Tortilla (vgn)
-- Wheat Tortilla (vgn)
-- Fire Roasted Tomato Salsa (vgn)
-Category: Pizza
-- Chorizo Pizza
-- Cheese Pizza (v)
-- Herb Breadsticks (vgn)
-Category: To Order
-- Chicken Stir Fry
-- Tofu Stir Fry (vgn)
-- Long Grain Rice (vgn)
-- Brown Rice (vgn)
-Category: Salads/Deli (West Side)
-- Caesar Salad (v)
-Category: Bakery
-- Swirled Herb Bread (v)
-- Lemon Jello Cake
-
-Dining Area: Ortega
-Category: Entree Specials
-- Teriyaki Bean Burger with Pineapple (v)
-- Teriyaki Burger with Pineapple
-- Macaroni & Cheese (v)
-Category: Entrees
-- Bacon Breakfast Burrito
-- Chicken Caesar Salad
-- Rainbow Salad w/Goddess Dressing (vgn)
-- Broccoli Soup (vgn)
-- Grilled Ham & Cheese Sandwich
-- Grilled Cheese Sandwich on Wheat (v)
-- Baked Pesto Pasta with Chicken
-- Mediterranean Turkey Wrap
-- Italian Tomato Panini (v)
-- Philly Cheesesteak Sandwich
-- Classic Burger
-- Veggie Burger (v)
-- Steak Burrito
-- Spicy Yellow Curry w/Tofu (w/nuts) (vgn)
-- Spicy Yellow Curry w/Chicken (w/nuts)
-- Chipotle BBQ Chicken & Potatoes
-Category: Sides
-- House Salad (vgn)
-- Hummus with Celery & Carrots (vgn)
-- Roasted Broccoli (vgn)
-- Roasted Citrus Thyme Carrots (v)
-- Seasoned Sweet Potato Halves (vgn)
-- Fries (vgn)
-- Lays Potato Chip (vgn)
-Category: Desserts
-- Cowboy Cookie Bar (v)
-- Rice Krispie Treat Bar
-- Snickerdoodle Cookie (v)
-Category: Whole Fruit
-- Apple (vgn)
-- Banana (vgn)
-- Orange (vgn)
-Category: Beverages
-- Water
-- Bubly Lime Sparkling Water (vgn)
-- Bubly Raspberry Sparkling Water (vgn)
-Category: Condiments
-- Balsamic Vinaigrette (vgn)
-- Ranch Dressing (v)
-- Mayonnaise (v)
-- Ketchup (vgn)
-- Mustard Packet (vgn)
-Category: Entree Specials
-- Sesame Beef with Broccoli
-- Teriyaki Bean Burger with Pineapple (v)
-- Teriyaki Burger with Pineapple
-- Macaroni & Cheese (v)
-Category: Entrees
-- Bacon Breakfast Burrito
-- Chicken Caesar Salad
-- Rainbow Salad w/Goddess Dressing (vgn)
-- Broccoli Soup (vgn)
-- Grilled Ham & Cheese Sandwich
-- Grilled Cheese Sandwich on Wheat (v)
-- Baked Pesto Pasta with Chicken
-- Mediterranean Turkey Wrap
-- Roasted Veggie Sandwich (v)
-- Philly Cheesesteak Sandwich
-- Classic Burger
-- Veggie Burger (v)
-- Steak Burrito
-- Spicy Yellow Curry w/Tofu (w/nuts) (vgn)
-- Spicy Yellow Curry w/Chicken (w/nuts)
-- Chipotle BBQ Chicken & Potatoes
-Category: Sides
-- House Salad (vgn)
-- Hummus with Celery & Carrots (vgn)
-- Roasted Broccoli (vgn)
-- Roasted Citrus Thyme Carrots (v)
-- Seasoned Sweet Potato Halves (vgn)
-- Fries (vgn)
-- Potato Chip (vgn)
-Category: Desserts
-- Cowboy Cookie Bar (v)
-- Rice Krispie Treat Bar
-- Snickerdoodle Cookie (v)
-Category: Whole Fruit
-- Apple (vgn)
-- Banana (vgn)
-- Orange (vgn)
-Category: Beverages
-- Bubly Lime Sparkling Water (vgn)
-- Bubly Raspberry Sparkling Water (vgn)
-- Water
-Category: Condiments
-- Balsamic Vinaigrette (vgn)
-- Ranch Dressing (v)
-- Mustard (vgn)
-- Ketchup (vgn)
-- Mayonnaise (v)
-
-Dining Area: Portola
-Category: Greens & Grains
-- Baked Sweet Potato (vgn)
-- Brown Rice (vgn)
-- Corn Chowder (v)
-Category: The Brick
-- Grilled BBQ Chicken Pizza
-- Bolognese Sauce with Pancetta
-- Farfalle Pasta (vgn)
-- Creamy Rosemary Sauce (v)
-- Fettuccine Pasta (vgn)
-Category: Salad Bar Featured Items
-- Organic Sugar Snap Pea
-- Ginger Sesame Tofu (vgn)
-- Couscous (vgn)
-Category: Bakery
-- Killer Brownie (w/nuts) (v)
-- Whole Wheat French Bread (v)
-Category: Chef's Choice
-- Tacos Al Pastor
-- Radish and Jalapeno Slaw (vgn)
-- Fire Roasted Tomato Chipotle Salsa (vgn)
-- Refried Beans (vgn)
-- Lettuce/Cheese/Tomatoes (vgn)
-Category: International
-- Vindaloo Chicken
-- Basmati Rice (vgn)
-- Indian Style Stew w/ButternutSquash(vgn)
-- Steamed Broccoli & Cauliflower (vgn)
-- California Club Roll
-
+const diningMenu = [
+Dining hall name not found
+"meals":[
+]
+}
+{
+"name": Carrillo
+"meals":[
+"Sticky Rice ", "Teriyaki Beef", "Stir Fry Noodles with Vegetables ", "Caesar Salad", "Herb Marinated Chicken Breast Sandwich", "Baby Carrots with Tarragon ", "Corn ", "Mashed Potatoes ", "Tomato Rice Soup ", "Linguini Pasta ", "Tomato Basil & Garlic Sauce ", "Whole Wheat Penne Pasta ", "Creamy Rosemary Sauce ", "Black Beans ", "Baked Potato ", "Baked Sweet Potato ", "Wild and Long Grain Rice Blend ", "Pepperoni Pizza", "Tomato Basil Pizza on Wheat ", "Cheese Pizza on White ", "Whole Wheat Roll ", "Banana Macadamia Nut Cake (w/nuts) ", ]
+}
+{
+"name": De La Guerra
+"meals":[
+"Vegetable Noodle Soup ", "Roast Beef", "Rice Pilaf ", "Vegetable Medley ", "Baked Potato ", "Chicken Sope", "Black Bean Tostada ", "Green Rice ", "Black Beans ", "Flour Tortilla ", "Corn Tortilla ", "Wheat Tortilla ", "Fire Roasted Tomato Salsa ", "Chorizo Pizza", "Cheese Pizza ", "Herb Breadsticks ", "Chicken Stir Fry", "Tofu Stir Fry ", "Long Grain Rice ", "Brown Rice ", "Caesar Salad ", "Swirled Herb Bread ", "Lemon Jello Cake", ]
+}
+{
+"name": Ortega
+"meals":[
+"Teriyaki Bean Burger with Pineapple ", "Teriyaki Burger with Pineapple", "Macaroni & Cheese ", "Bacon Breakfast Burrito", "Chicken Caesar Salad", "Rainbow Salad w/Goddess Dressing ", "Broccoli Soup ", "Grilled Ham & Cheese Sandwich", "Grilled Cheese Sandwich on Wheat ", "Baked Pesto Pasta with Chicken", "Mediterranean Turkey Wrap", "Italian Tomato Panini ", "Philly Cheesesteak Sandwich", "Classic Burger", "Veggie Burger ", "Steak Burrito", "Spicy Yellow Curry w/Tofu (w/nuts) ", "Spicy Yellow Curry w/Chicken (w/nuts)", "Chipotle BBQ Chicken & Potatoes", "House Salad ", "Hummus with Celery & Carrots ", "Roasted Broccoli ", "Roasted Citrus Thyme Carrots ", "Seasoned Sweet Potato Halves ", "Fries ", "Lays Potato Chip ", "Cowboy Cookie Bar ", "Rice Krispie Treat Bar", "Snickerdoodle Cookie ", "Apple ", "Banana ", "Orange ", "Water", "Bubly Lime Sparkling Water ", "Bubly Raspberry Sparkling Water ", "Balsamic Vinaigrette ", "Ranch Dressing ", "Mayonnaise ", "Ketchup ", "Mustard Packet ", ]
+}
+{
+"name": Portola
+"meals":[
+"Baked Sweet Potato ", "Brown Rice ", "Corn Chowder ", "Grilled BBQ Chicken Pizza", "Bolognese Sauce with Pancetta", "Farfalle Pasta ", "Creamy Rosemary Sauce ", "Fettuccine Pasta ", "Organic Sugar Snap Pea", "Ginger Sesame Tofu ", "Couscous ", "Killer Brownie (w/nuts) ", "Whole Wheat French Bread ", "Tacos Al Pastor", "Radish and Jalapeno Slaw ", "Fire Roasted Tomato Chipotle Salsa ", "Refried Beans ", "Lettuce/Cheese/Tomatoes ", "Vindaloo Chicken", "Basmati Rice ", "Indian Style Stew w/ButternutSquash", "Steamed Broccoli & Cauliflower ", "California Club Roll", ]
+}
+]
