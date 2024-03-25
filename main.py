2472724a75e6633281e5a4c909a4e2ddfd940076
Justin Lang <jlang61@ucsb.edu>
2024-03-25 16:44:59 -0700

    working py file

--- main.py
+++ main.py
@@ -36,31 +36,30 @@ for section in sections:
     if dining_hall_alert:
         dining_hall_name = dining_hall_alert.text.strip()
         print('{')
-        print("\"name\":", dining_hall_name)
-    else:
-        print("Dining hall name not found")
+        print("\"name\":", "\"" + dining_hall_name + "\", ", end = "")
+        print()
 
     # Extract the meal information
-    meal_info = section.find(class_='panel-body')
-    if meal_info:
-        meal_name = meal_info.find_previous(class_='panel-heading').text.strip()
-        # print("Meal:", meal_name)
-        
-        # Extract the menu items
-        menu_items = meal_info.find_all('dl')
-        print("\"meals\":["),
-        for item in menu_items:
-            category = item.find('dt').text.strip()
-            # print("Category:", category)
-            items = item.find_all('dd')
-            for item_text in items:
-                strip_text = item_text.text.strip()
-                corrected_text = strip_text
-                for take_out in ["(vgn)", "(v)"]:
-                    corrected_text = corrected_text.replace(take_out,"")
-                print("\"" + corrected_text + "\", ", end = ""),
-        print("]")
-    else:
-        print("No meal information available")
-    print("}")
+        meal_info = section.find(class_='panel-body')
+        if meal_info:
+            meal_name = meal_info.find_previous(class_='panel-heading').text.strip()
+            # print("Meal:", meal_name)
+            
+            # Extract the menu items
+            menu_items = meal_info.find_all('dl')
+            print("\"meals\":["),
+            for item in menu_items:
+                category = item.find('dt').text.strip()
+                # print("Category:", category)
+                items = item.find_all('dd')
+                for item_text in items:
+                    strip_text = item_text.text.strip()
+                    corrected_text = strip_text
+                    for take_out in ["(vgn)", "(v)"]:
+                        corrected_text = corrected_text.replace(take_out,"")
+                    print("\"" + corrected_text + "\", ", end = ""),
+            print("]")
+        else:
+            print("No meal information available")
+        print("},")
 print("]")
