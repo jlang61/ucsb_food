0f24ae8bcc0976f16d452574c8a09f28e04e6386
Justin Lang <jlang61@ucsb.edu>
2024-03-25 23:45:38 +0000

    Update foodData.ts

--- foodData.ts
+++ foodData.ts
@@ -1,1 +1,22 @@
-g
+const diningMenu = [
+{
+"name": "Carrillo", 
+"meals":[
+"Sticky Rice ", "Teriyaki Beef", "Stir Fry Noodles with Vegetables ", "Caesar Salad", "Herb Marinated Chicken Breast Sandwich", "Baby Carrots with Tarragon ", "Corn ", "Mashed Potatoes ", "Tomato Rice Soup ", "Linguini Pasta ", "Tomato Basil & Garlic Sauce ", "Whole Wheat Penne Pasta ", "Creamy Rosemary Sauce ", "Black Beans ", "Baked Potato ", "Baked Sweet Potato ", "Wild and Long Grain Rice Blend ", "Pepperoni Pizza", "Tomato Basil Pizza on Wheat ", "Cheese Pizza on White ", "Whole Wheat Roll ", "Banana Macadamia Nut Cake (w/nuts) ", ]
+},
+{
+"name": "De La Guerra", 
+"meals":[
+"Vegetable Noodle Soup ", "Roast Beef", "Rice Pilaf ", "Vegetable Medley ", "Baked Potato ", "Chicken Sope", "Black Bean Tostada ", "Green Rice ", "Black Beans ", "Flour Tortilla ", "Corn Tortilla ", "Wheat Tortilla ", "Fire Roasted Tomato Salsa ", "Chorizo Pizza", "Cheese Pizza ", "Herb Breadsticks ", "Chicken Stir Fry", "Tofu Stir Fry ", "Long Grain Rice ", "Brown Rice ", "Caesar Salad ", "Swirled Herb Bread ", "Lemon Jello Cake", ]
+},
+{
+"name": "Ortega", 
+"meals":[
+"Teriyaki Bean Burger with Pineapple ", "Teriyaki Burger with Pineapple", "Macaroni & Cheese ", "Bacon Breakfast Burrito", "Chicken Caesar Salad", "Rainbow Salad w/Goddess Dressing ", "Broccoli Soup ", "Grilled Ham & Cheese Sandwich", "Grilled Cheese Sandwich on Wheat ", "Baked Pesto Pasta with Chicken", "Mediterranean Turkey Wrap", "Italian Tomato Panini ", "Philly Cheesesteak Sandwich", "Classic Burger", "Veggie Burger ", "Steak Burrito", "Spicy Yellow Curry w/Tofu (w/nuts) ", "Spicy Yellow Curry w/Chicken (w/nuts)", "Chipotle BBQ Chicken & Potatoes", "House Salad ", "Hummus with Celery & Carrots ", "Roasted Broccoli ", "Roasted Citrus Thyme Carrots ", "Seasoned Sweet Potato Halves ", "Fries ", "Lays Potato Chip ", "Cowboy Cookie Bar ", "Rice Krispie Treat Bar", "Snickerdoodle Cookie ", "Apple ", "Banana ", "Orange ", "Water", "Bubly Lime Sparkling Water ", "Bubly Raspberry Sparkling Water ", "Balsamic Vinaigrette ", "Ranch Dressing ", "Mayonnaise ", "Ketchup ", "Mustard Packet ", ]
+},
+{
+"name": "Portola", 
+"meals":[
+"Baked Sweet Potato ", "Brown Rice ", "Corn Chowder ", "Grilled BBQ Chicken Pizza", "Bolognese Sauce with Pancetta", "Farfalle Pasta ", "Creamy Rosemary Sauce ", "Fettuccine Pasta ", "Organic Sugar Snap Pea", "Ginger Sesame Tofu ", "Couscous ", "Killer Brownie (w/nuts) ", "Whole Wheat French Bread ", "Tacos Al Pastor", "Radish and Jalapeno Slaw ", "Fire Roasted Tomato Chipotle Salsa ", "Refried Beans ", "Lettuce/Cheese/Tomatoes ", "Vindaloo Chicken", "Basmati Rice ", "Indian Style Stew w/ButternutSquash", "Steamed Broccoli & Cauliflower ", "California Club Roll", ]
+},
+]
