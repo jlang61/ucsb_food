a45ec6f03d38e2cd49e851a5983ddccd1a9f3baa
Justin Lang <jlang61@ucsb.edu>
2024-03-25 13:55:59 -0700

    changing workflow and main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ desired_entree = "Teriyaki Bean Burger with Pineapple (v)"  # Change this to the
 entrees = soup.find_all('dd')
 
 # Check if the desired entree is in the list of entrees
-if any(desired_entree in entree.text for entree in entrees):
-    print("The entree '{}' is served at {}.".format(desired_entree, dining_hall_info))
-else:
-    print("The entree '{}' is not served at {}.".format(desired_entree, dining_hall_info))
+# if any(desired_entree in entree.text for entree in entrees):
+#     print("The entree '{}' is served at {}.".format(desired_entree, dining_hall_info))
+# else:
+#     print("The entree '{}' is not served at {}.".format(desired_entree, dining_hall_info))
