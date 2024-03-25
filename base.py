f89ca3a6a43f86f8c462bdaabc391c4ec652fd62
Justin Lang <jlang61@ucsb.edu>
2024-03-24 18:31:32 -0700

    testing github actions

---
+++ base.py
@@ -0,0 +1,23 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+# Fetch HTML content from the URL
+url = "https://apps.dining.ucsb.edu/menu/day"
+page = urlopen(url)
+html = page.read()
+
+# Parse the HTML content
+soup = BeautifulSoup(html, 'html.parser')
+
+# Find the dining hall information
+dining_hall_info = soup.find('div', class_='alert alert-danger').h5.text
+
+# Find the entree you are interested in
+desired_entree = "Teriyaki Bean Burger with Pineapple (v)"  # Change this to the desired entree
+entrees = soup.find_all('dd')
+
+# Check if the desired entree is in the list of entrees
+if any(desired_entree in entree.text for entree in entrees):
+    print("The entree '{}' is served at {}.".format(desired_entree, dining_hall_info))
+else:
+    print("The entree '{}' is not served at {}.".format(desired_entree, dining_hall_info))
