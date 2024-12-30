5f24fe5150ba2d6700336355d4c29b89bf207a09
Justin Lang <jlang61@ucsb.edu>
2024-12-30 07:20:52 +0000

    Update foodData.ts

--- client/data/foodData.ts
+++ client/data/foodData.ts
@@ -1,30 +1,5 @@
 const diningMenu = [
 {
-"date": "2024-12-29",
-"dining_halls": [
-{
-"name": "Carrillo",
-"meals": {
-}
-},
-{
-"name": "De La Guerra",
-"meals": {
-}
-},
-{
-"name": "Ortega",
-"meals": {
-}
-},
-{
-"name": "Portola",
-"meals": {
-}
-},
-],
-},
-{
 "date": "2024-12-30",
 "dining_halls": [
 {
@@ -170,6 +145,34 @@ const diningMenu = [
 {
 "name": "Portola",
 "meals": {
+}
+},
+],
+},
+{
+"date": "2025-01-05",
+"dining_halls": [
+{
+"name": "Carrillo",
+"meals": {
+"Dinner": ["Sticky Rice", "Chicken General Tso's", "Chinese Style Tofu & Vegetables", "Split Pea Soup", "Kale & Arugula Salad w/Cranberries", "Turkey Breast", "Turkey Gravy", "Sauteed Zucchini Sticks", "Baby Carrots with Tarragon", "Herb Stuffing", "Mashed Potatoes", "Cranberry Sauce", "Fettucine Alfredo", "Bowtie Pasta", "Marinara Sauce", "Baked Potato", "Baked Sweet Potato", "Pinto Beans", "Long Grain Rice", "Ham & Pineapple", "Cheese Pizza on White", "Artichoke Pizza with Pesto on Wheat", "Hawaiian Sweet Bread", "Tres Leches Cake"],
+}
+},
+{
+"name": "De La Guerra",
+"meals": {
+"Dinner": ["Vegetable Noodle Soup", "Roast Beef", "Vegetable Turnover", "Rice Pilaf", "Vegetable Medley", "Baked Potato", "Pork Chile Verde Tostada", "Bean & Rice Tostada", "Green Rice", "Refried Beans", "Flour Tortilla", "Corn Tortilla", "Wheat Tortilla", "Fire Roasted Tomato Salsa", "Sausage Pizza", "Pesto Cheese Pizza", "Herb Breadsticks", "Chicken & Vegetable Stir Fry", "Tofu Stir Fry", "Long Grain Rice", "Brown Rice", "Tabouli Salad", "Swirled Herb Bread", "Lemon Jello Cake"],
+}
+},
+{
+"name": "Ortega",
+"meals": {
+}
+},
+{
+"name": "Portola",
+"meals": {
+"Dinner": ["Baked Sweet Potato", "Yellow Rice", "Roasted Butternut Squash Soup", "Turmeric Roasted Cauliflower", "Pickled Asian Mushroom", "Bulgur Wheat", "Vegetable Stromboli", "Creamy Pesto Sauce", "Penne Pasta", "Mushroom Ragout Over Herb Polenta", "Hawaiian Sweet Bread", "Banana Cream Pie", "Chicken Cacciatore", "Pasta with Olive Oil", "Risotto with Fresh Vegetables", "Steamed Broccoli & Cauliflower", "Moroccan Braised Beef", "Couscous", "Spanish Spiced Lentils", "Spinach with Fresh Garlic & Ginger", "Spicy Shrimp Sushi Plate", "Cucumber & Avocado Roll"],
 }
 },
 ],
