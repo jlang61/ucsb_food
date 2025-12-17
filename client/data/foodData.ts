c676febb6368e7ca5d4313cf8d0bfdc5562cdef3
Justin Lang <jlang61@ucsb.edu>
2025-12-17 07:26:20 +0000

    Update foodData.ts

--- client/data/foodData.ts
+++ client/data/foodData.ts
@@ -1,30 +1,5 @@
 const diningMenu = [
 {
-"date": "2025-12-16",
-"dining_halls": [
-{
-"name": "Carrillo",
-"meals": {
-}
-},
-{
-"name": "De La Guerra",
-"meals": {
-}
-},
-{
-"name": "Ortega",
-"meals": {
-}
-},
-{
-"name": "Portola",
-"meals": {
-}
-},
-],
-},
-{
 "date": "2025-12-17",
 "dining_halls": [
 {
@@ -160,9 +135,31 @@ const diningMenu = [
 {
 "name": "De La Guerra",
 "meals": {
-"Lunch": ["Vegetable Soup", "Spicy Meatball Sandwich", "Fresh Steamed Carrots", "Baked Potato", "Chicken and Bean Burrito", "Potato Taco", "Mexican Rice", "Pinto Beans", "Flour Tortilla", "Corn Tortilla", "Wheat Tortilla", "Roasted Tomato and Chile Salsa", "Pepperoni Pizza", "Tomato Basil Pizza", "Herb Breadsticks", "Teriyaki Broccoli Beef Stir Fry", "Vegetable Chop Suey", "Long Grain Rice", "Brown Rice", "Cheeseburger", "Malibu Burger", "Hot Dogs", "Krinkle Cut French Fries", "Egg Salad", "Jicama Salad", "Three Grain Roll", "Killer Brownie", "Lemon Jello Cake"],
-"Dinner": ["Vegetable Beef Soup", "Sweet & Sour Glazed Chicken", "Pineapple Rice", "Fresh Broccoli Spears", "Steamed Red Potatoes with Parsley", "Baked Potato", "Turkey Hard Taco", "zTaco De Crema", "Mexican Rice", "Pinto Beans", "Flour Tortilla", "Corn Tortilla", "Wheat Tortilla", "Black Bean Salsa", "Pepperoni Pizza", "Tomato Basil Pizza", "Herb Breadsticks", "Chicken Chow Mein", "Vegetable Chow Mein", "Long Grain Rice", "Brown Rice", "Chicken Chow Mein", "Caesar Salad", "Hawaiian Sweet Bread", "French Peach Crumb Pie"],
-"Late Night": ["Cheeseburger", "Malibu Burger", "Grilled Cheese on White", "Grilled Turkey & Swiss on Wheat", "Krinkle Cut French Fries", "zSoup", "zLate Night Salad Bar", "Oatmeal Raisin Walnut Bar (w/nuts)"],
+}
+},
+{
+"name": "Ortega",
+"meals": {
+}
+},
+{
+"name": "Portola",
+"meals": {
+}
+},
+],
+},
+{
+"date": "2025-12-23",
+"dining_halls": [
+{
+"name": "Carrillo",
+"meals": {
+}
+},
+{
+"name": "De La Guerra",
+"meals": {
 }
 },
 {
